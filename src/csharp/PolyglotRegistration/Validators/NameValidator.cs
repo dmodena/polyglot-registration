e8a8b037789a8f6beb36f55823b73d63b5098c34
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-10-28 17:03:23 -0500

    Added name validation in CSharp

--- src/csharp/PolyglotRegistration/Validators/NameValidator.cs
+++ src/csharp/PolyglotRegistration/Validators/NameValidator.cs
@@ -1,10 +1,38 @@
-﻿namespace PolyglotRegistration.Validators
+﻿using System.Text.RegularExpressions;
+
+namespace PolyglotRegistration.Validators
 {
     public class NameValidator : IValidator<string>
     {
         public bool IsValid(string inputValue)
         {
-            return true;
+            if (Regex.IsMatch(inputValue, @"^[a-zA-Z- ]+$") && inputValue.Contains(" ") && char.IsUpper(inputValue[0]))
+            {
+                for (int i = 1; i < inputValue.Length; i++)
+                {
+                    if (inputValue[i] == ' ' || inputValue[i] == '-')
+                    {
+                        if (char.IsUpper(inputValue[i + 1]))
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (!char.IsLower(inputValue[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
+
