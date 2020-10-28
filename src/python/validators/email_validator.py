03b6c3911d0bd591c73542fd578f9e083a1a7ab7
Pierre <pierre.git@posteo.de>
2020-10-28 23:18:53 +0100

    adds an email validator

--- src/python/validators/email_validator.py
+++ src/python/validators/email_validator.py
@@ -1,3 +1,20 @@
 class EmailValidator:
     def is_valid(self, email):
+        if email.count("@") != 1:
+            return False
+
+        if email[0] == "." or email[-1] == ".":
+            return False
+
+        if email[-1] == "@":
+            return False
+
+        if not email.islower():
+            return False
+
+        for c in email:
+            if not (c.isalpha() or c.isdigit() or c == "." or c == "@"):
+                return False
+
         return True
+
