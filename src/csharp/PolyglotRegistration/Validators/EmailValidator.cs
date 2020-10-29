40fdb40325979191aa5dd1e148104246a7cccced
pk-95 <60467716+pk-95@users.noreply.github.com>
2020-10-29 15:18:45 +0530

    Update EmailValidator.cs

--- src/csharp/PolyglotRegistration/Validators/EmailValidator.cs
+++ src/csharp/PolyglotRegistration/Validators/EmailValidator.cs
@@ -1,10 +1,16 @@
-﻿namespace PolyglotRegistration.Validators
+using System;
+using System.Text.RegularExpressions;
+namespace PolyglotRegistration.Validators
 {
     public class EmailValidator : IValidator<string>
     {
         public bool IsValid(string inputValue)
         {
-            return true;
+            Regex regex = new Regex(@"^[a-z0-9][a-z0-9.]*@[a-z0-9]*\.?[a-z0-9.]*[a-z0-9]$");
+            Match match = regex.Match(inputValue);
+            if (match.Success)
+                return true;
+            return false;
         }
     }
 }
