d9bf72d4bc489d9e58f20e9f7690cd7ec7938dc1
yuanLeeMidori <midori3113133@gmail.com>
2020-10-28 21:07:59 -0400

    fix: add python name validator

--- src/python/validators/name_validator.py
+++ src/python/validators/name_validator.py
@@ -1,3 +1,21 @@
+import re
+
 class NameValidator:
     def is_valid(self, name):
-        return True
+        namePattern = "^[A-Z][a-z]{1,}$"
+        if " " in name:
+            name_words = name.split(" ")
+            for name_word in name_words:
+                if "-" in name_word:
+                    words_with_dash = name_word.split("-")
+                    for word_with_dash in words_with_dash:
+                        if not (re.match(namePattern, word_with_dash)):                             
+                            return False
+                else:
+                    if not (re.match(namePattern, name_word)):          
+                        return False
+            return True    
+        else:
+            return False
+        
+
